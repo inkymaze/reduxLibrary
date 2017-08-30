a34af58b9b5bbdbd10b677f50fb2373481994bd1
inkymaze <nickwcannon@gmail.com>
2017-08-30 16:44:13 -0700

    initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import BookList from '../containers/book-list';
+
+// a container is a normal react component that gets bonded to the State
+// whenevewr state changes the component changes as well
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+      </div>
     );
   }
 }
